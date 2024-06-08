7910a0277f09982fdf48115abcc6707e4aaea136
tiendepzaii <115964943+tiendepzaii@users.noreply.github.com>
2024-06-08 21:17:21 +0700

    Add files via upload

---
+++ Show.php
@@ -0,0 +1,92 @@
+<?php 
+  include "header.php";
+?>
+   
+    <div class="app_container">
+        <div class="cartegory-top row">
+            <p><a href="index.html">Trang chủ</a></p> <span>&#8594;</span>
+            <p>Tin Tức</p> <span>&#10230;</span>
+            <p>ƯU ĐÃI THÁNG 7: MUA 1 TẶNG 1 - NHẬN NGAY QUÀ TẶNG 0Đ</p>
+        </div>
+        <div class="row">
+            <div class="grid__row app_content">
+                <div class="grid__colum-2">
+                    <nav class="category">
+                        <h3 class="category_heading">
+                            <i class="category_heading-icon fa-solid fa-list"></i> Danh mục
+                        </h3>
+                        <ul class="category_list">
+                            <li class="category_item category_item--active">
+                                <a href="" class="category_item_link">Sự kiện thời trang </a>
+                            </li>
+                            <li class="category_item ">
+                                <a href="" class="category_item_link">Hoặt động cộng đồng</a>
+                            </li>
+                            <li class="category_item ">
+                                <a href="" class="category_item_link">Tin nội bộ</a>
+                            </li>
+                            <li class="category_item ">
+                                <a href="" class="category_item_link">Blog chia sẻ</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+
+                <div class="grid_new-10">
+                    <div class="grid_new-6">
+                        <div class="content-news">
+                            <ul class="list-inline">
+                                <li class="col-md-3 col-sm-4 col-xs-12">
+                                    <a href=""><img src="assets/img/show1.png"></a>
+                                </li>
+                                <li class="col-md-9 col-sm-8 col-xs-12">
+                                    <h4><a href="">HARMONY - SS23 FASHION SHOW | ĐÁNH THỨC THANH ÂM TRONG BẠN</a>
+                                    </h4>
+                                    <div class="news_detail">Vào tối 25 tháng 3 vừa qua, thương hiệu thời trang hàng đầu Việt Nam IVY moda đã có buổi ra mắt BST Spring Summer 2023 mang tên Harmony diễn ra tại Thành phố Hồ Chí Minh. Sự kiện quy tụ nhiều mỹ nhân hàng đầu showbiz Việt - mỗi người diện một thiết kế từ BST mới, đại diện cho một thanh âm của tính nữ, màn đọ sắc này được đông đảo khán giả và giới mộ điệu thời trang quan tâm. Với quy mô hoành tráng cùng sự quy tụ của nhiều người mẫu nữ và sự góp mặt của dàn khách mời nổi tiếng</div>
+                                </li>
+                            </ul>
+              
+                            <ul class="list-inline">
+                                <li class="col-md-3 col-sm-4 col-xs-12">
+                                    <a href=""><img src="assets/img/show2.png"></a>
+                                </li>
+                                <li class="col-md-9 col-sm-8 col-xs-12">
+                                    <h4><a href="">TIMELESS | FW22 FASHION SHOW - VẺ ĐẸP TỎA RẠNG VƯỢT THỜI GIAN</a>
+                                    </h4>
+                                    <div class="news_detail">Timeless - show diễn thời trang Thu Đông 2022 cũng là show thứ 20 của IVY moda. Show diễn được tổ chức trong khuôn viên rộng 23.000m2 của trường Đại học VinUni, Hà Nội - với lối kiến trúc mang âm hưởng sang trọng cùng hơi thở châu Âu giữa lòng Hà Nội, đem tới cho khán giả trải nghiệm bữa tiệc thời trang khác biệt, mới mẻ và đầy cảm hứng. Đồng thời phá vỡ các khuôn mẫu sao cho trang phục trở nên linh hoạt và có tính ứng dụng cao.</div>
+                                </li>
+                            </ul>
+                
+                            <ul class="list-inline">
+                                <li class="col-md-3 col-sm-4 col-xs-12">
+                                    <a href=""><img src="assets/img/show3.png"></a>
+                                </li>
+                                <li class="col-md-9 col-sm-8 col-xs-12">
+                                    <h4><a href="">Digital Show 2022</a>
+                                    </h4>
+                                    <div class="news_detail">Vào tối 20h00 ngày 09 tháng 04 năm 2022 tới đây, Thương hiệu thời trang hàng đầu Việt Nam IVY moda sẽ phát sóng show diễn thời trang phiên bản “công nghệ” mang tên “Mơ nhuộm nắng” SS 2022 trên fanpage chính thức và các nền tảng online khác. Đây cũng là lần đầu tiên, một Thương hiệu thời trang Việt Nam tiên phong mang đến những trải nghiệm mới mẻ của Digital Show mà không làm mất đi tinh thần và sự sôi động của sàn Runway.</div>
+                                </li>
+                            </ul>
+                            
+
+                        </div>
+                       
+                    </div>
+                    <div class="grid_new-4">
+                        <section class="bg-before">
+                            <a href="" alt="News Sale">
+                                <img src="assets/img/sale1.png" alt="News Sale">
+                            </a>
+                        </section>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+
+    <?php 
+    include "footer.php";
+    ?>
